342e43f42052c91c738f1d020ae4b41b10310800
TuvalYulevich <122490575+TuvalYulevich@users.noreply.github.com>
2023-06-15 11:40:31 +0300

    Add files via upload

---
+++ express.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+var express = require("express");
+var path = require("path");
+var connectDB = require("./Backend/config/database");
+
+// connect to db
+connectDB();
+
+// Init app
+app = express();
+
+// set ejs engine
+app.set("frontend", path.join(__dirname, "frontend"));
+app.set("view engine", "ejs");
+
+// Set folders
+app.use(express.static(__dirname + '/views/public'));
+
+// Set Routers
+var pages = require('./Backend/config/routePages');
+var adminPages = require('./Backend/config/admin_pages');
+
+app.use('/admin/pages', adminPages);
+app.use('/', pages);
+// Start Server
+const port = 3300;
+app.listen(port,function(){
+    console.log("Server started on port", port);
+});
